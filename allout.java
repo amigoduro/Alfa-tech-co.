430191300fc2a0c6397beb3c32f5de3671984e9f
Cadet Adarsh <pal.adarsh10205@gmail.com>
2025-09-18 10:33:06 +0530

    Create allout.java
    this code i just try for the understanding of (merging) class and also this method is same as inheritance infact the only thing is change is extend classes and also in this code changes will take place ASAP

---
+++ allout.java
@@ -0,0 +1,70 @@
+class alfa
+{
+	int a,b;
+	void addition(int x,int y)
+	{
+		a=x;
+		b=y;
+	}
+	int sum()
+	{
+		return(a+b);
+	}
+}
+
+class bita extends alfa
+{
+	int c,d,e;
+	void subtract(int x)
+	{
+		c=x;
+		//d=y;
+	}
+	int minus()
+	{
+		return(sum()-c);
+	}
+}
+
+class charli extends bita
+{
+	int f,g,k;
+	void multiply(int x)
+	{
+		f=x;
+		//g=y;
+	}
+	int multi()
+	{
+		return(minus()*f);
+	}
+}
+
+class delta extends charli
+{
+	int l,m,n;
+	void divide(int x)
+	{
+		l=x;
+		//m=y;
+	}
+	int div()
+	{
+		return(multi()/l);
+	}
+}
+class allout
+{
+	public static void main (String args[])
+	{
+		delta i = new delta();
+		i.addition(10,10);
+		i.subtract(5);
+		i.multiply(10);
+		i.divide(10);
+		System.out.println("Answer's of addition are =  "+i.sum());
+		System.out.println("Answer's of subtraction are =  "+i.minus());
+		System.out.println("Answer's of multiplication are =  "+i.multi());
+		System.out.println("Answer's of divide are =  "+i.div());
+	}
+}

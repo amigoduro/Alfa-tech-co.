5040d6c542de2a3f772e9fdcc5b8d670d2a1822c
Cadet Adarsh <pal.adarsh10205@gmail.com>
2025-10-05 12:02:02 +0530

    Update PalindromeCheck.java

--- PalindromeCheck.java
+++ PalindromeCheck.java
@@ -1,5 +1,6 @@
 // PalindromeCheck.java
 import java.util.Scanner;
+                // also we can use the math.h file for the easy connections
 
 public class PalindromeCheck {
     public static void main(String[] args) {

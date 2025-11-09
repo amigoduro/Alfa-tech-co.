db12e4dbc763ded2e87cdbc989fdcc9d93a31111
Cadet Adarsh <pal.adarsh10205@gmail.com>
2025-11-09 14:08:04 +0530

    Update Fibb3.java

--- Fibb3.java
+++ Fibb3.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Fibb3 {
     // returns {F(n), F(n+1)}
-    private static BigInteger[] fibPair(long n) {
+    private static BigInteger[] fibPair(long n)
+                                                {
         if (n == 0) return new BigInteger[]{BigInteger.ZERO, BigInteger.ONE};
         BigInteger[] half = fibPair(n >> 1);
         BigInteger a = half[0]; // F(k)

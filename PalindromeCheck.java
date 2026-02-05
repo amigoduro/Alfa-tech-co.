dcf9f8012c6ac5a07ee68e56399a7e5d34cbcf0c
Cadet Adarsh <pal.adarsh10205@gmail.com>
2026-02-05 09:17:23 +0530

    Fix formatting in main method of PalindromeCheck

--- PalindromeCheck.java
+++ PalindromeCheck.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class PalindromeCheck 
                             {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+                              {
       
         Scanner sc = new Scanner(System.in);
 

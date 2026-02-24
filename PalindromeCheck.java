a7a537e4bf3bc878de09450e45796b868a723f74
Cadet Adarsh <pal.adarsh10205@gmail.com>
2026-02-25 02:46:37 +0530

    Update PalindromeCheck.java

--- PalindromeCheck.java
+++ PalindromeCheck.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 
 public class PalindromeCheck    
-                            {
+                            {   
     public static void main(String[] args)
                               {
       

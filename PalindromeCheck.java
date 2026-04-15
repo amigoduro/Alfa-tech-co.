2b15d8bd38d90310ac766dc986a99042d8a9e61f
Cadet Adarsh <pal.adarsh10205@gmail.com>
2026-04-15 20:40:03 +0530

    Remove unnecessary blank line in main method

--- PalindromeCheck.java
+++ PalindromeCheck.java
@@ -6,7 +6,6 @@ public class PalindromeCheck
                             {   
     public static void main(String[] args)
                               {
-      
         Scanner sc = new Scanner(System.in);
 
         // Input from user

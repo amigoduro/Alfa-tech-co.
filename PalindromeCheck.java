7e5bf1fb621f2b2ded8be54d1a55ca7f3baa9bf4
Cadet Adarsh <pal.adarsh10205@gmail.com>
2026-06-21 00:25:57 +0530

    Update PalindromeCheck.java

--- PalindromeCheck.java
+++ PalindromeCheck.java
@@ -6,7 +6,7 @@ public class PalindromeCheck
                             {   
     public static void main(String[] args)      
                               {      
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);   
         // Input from user
         System.out.print("Enter a string or number: ");
       

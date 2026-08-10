f3770f0c6fb5e4a1361642ee4015c24b3d8a1858
Cadet Adarsh <pal.adarsh10205@gmail.com>
2026-08-10 21:59:38 +0530

    Update Fibb3.java

--- Fibb3.java
+++ Fibb3.java
@@ -6,7 +6,8 @@ public class Fibb3  {
     // returns {F(n), F(n+1)}
     private static BigInteger[] fibPair(long n)
        
-                                                        {     
+                            
+                                {     
                                                     
         if (n == 0) return new BigInteger[]{BigInteger.ZERO, BigInteger.ONE};
         BigInteger[] half = fibPair(n >> 1);
